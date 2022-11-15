aad10c3c789fa9a0b992a522d172020e6f7915b5
KatOli4ka <katoli4ka87@mail.ru>
2022-11-15 19:15:59 +0500

    задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,24 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Задача 1");
         int[] arr = generateRandomArray();
 
        }
+
     public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
         int[] arr = new int[30];
-        int i;
-        for (i = 0; i < arr.length; i++) {
+        int sum=0;
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = random.nextInt(100_000) + 100_000;
         }
-        System.out.println(arr[i-1]);
-        return arr;
+        for (int i: arr)
+            sum += i;
 
+        System.out.println(sum);
+
+
+        return arr;
     }
 
 
